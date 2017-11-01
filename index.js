b7ab4bb422852bacd0192c4cd1de842f6431e91a
Ethan Arterberry <ethanarterberry@gmail.com>
2017-11-01 21:53:56 +0000

    woo! you can turn it off now

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-var monitor = function() {
-    var errorCallback = function(e) {
-        console.log("whoops", e)
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        
+var context = new AudioContext();
+var gainNode = context.createGain();
+var microphone;
+var initialized = false;
+
+document.getElementById("init").onclick = function() {
+    if (initialized != true) {
+        var errorCallback = function(e) {
+            console.log("whoops", e);
+        }
+        
+        navigator.getUserMedia({video: false, audio: true}, function(stream) {
+            microphone = context.createMediaStreamSource(stream);
+            gainNode.gain.value = 1;
+            
+            microphone.connect(gainNode);
+            gainNode.connect(context.destination);
+            initialized = true;
+            document.getElementById("init").innerText = "stop";
+        }, errorCallback);
     }
     
-    window.AudioContext = window.AudioContext ||
-                          window.webkitAudioContext;
-    var context = new AudioContext();
-    
-    navigator.getUserMedia({video: false, audio: true}, function(stream) {
-        var microphone = context.createMediaStreamSource(stream);
-        
-        microphone.connect(context.destination);
-    }, errorCallback)
+    if (document.getElementById("init").innerText == "start") {
+        gainNode.connect(context.destination);
+        document.getElementById("init").innerText = "stop";
+    } else if (document.getElementById("init").innerText == "stop") {
+        gainNode.disconnect(context.destination);
+        document.getElementById("init").innerText = "start";
+    }
 }
